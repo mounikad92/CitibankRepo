c08d90c7755c9e38612693e88b2b6de35bed297d
mounikad92 <mounikadevops992@gmail.com>
2022-07-14 17:31:17 +0530

    application.java file is added into development branch

---
+++ Application.java
@@ -0,0 +1,19 @@
+class Main{
+public static void main(String[] args){
+ File file=new File()[D"")[D[Dnewfile.txt");
+try{
+boolean value=file.cresteNewFile();
+if()[D[D()[Dvalue)
+{
+System.out.println("the new file is created");
+}
+else
+{
+System.out.println("the file already exists");
+}
+}
+catch(Exception e){
+e.getStacktrace();
+}
+}
+}
